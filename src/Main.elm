433e8d28cbb4b5063b34f83d30403d3c5b8d50ae
Andreas Hellwig <a5308y@gmail.com>
2018-10-13 12:02:10 +0200

    Improve debug output

--- src/Main.elm
+++ src/Main.elm
@@ -28,19 +28,19 @@ main =
 view : Model -> Html Msg
 view model =
     div []
-        [ h2 [] [ text "CRDT" ]
-        , input [ onInput UpdateCRDT, value (CRDT.toString model.crdt) ] []
-        , div [] [ text (CRDT.toString model.crdt) ]
-        , div [] (List.map (\operation -> div [] [ text (Debug.toString operation) ]) model.crdt.operations)
-        , h2 [] [ text "control" ]
+        [ h2 [] [ text "control" ]
         , input [ onInput UpdateCRDT, value model.control ] []
         , div [] [ text model.control ]
-        , br [] []
         , if CRDT.toString model.crdt == model.control then
             strong [] [ text "Result matches!" ]
 
           else
             strong [] [ text "Out of Sync!" ]
+        , h2 [] [ text "CRDT" ]
+        , input [ onInput UpdateCRDT, value (CRDT.toString model.crdt) ] []
+        , div [] [ text (CRDT.toString model.crdt) ]
+        , div [] (List.map (\operation -> div [] [ text (Debug.toString operation) ]) model.crdt.operations)
+        , br [] []
         ]
 
 
